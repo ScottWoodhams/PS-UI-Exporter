049243aaa75f9de132fcddba28b8ca16bfda7f60
Scott Woodhams <ScottWoodhams@hotmail.com>
2022-02-13 10:51:51 +0000

    fixed issue with plugin trying to run when no document is open

--- src/Panels/InitPanel.tsx
+++ src/Panels/InitPanel.tsx
@@ -1,32 +1,49 @@
-import React from 'react';
-import {action, app, core, ExecuteAsModalOptions} from "photoshop";
-import UILayerData, { LayerDataInit } from '../typescript/UILayerData';
-import { InitLayers, WriteToMetaData } from "../typescript/Metadata";
+import React, { useEffect, useState } from 'react';
+import { action, app, core, ExecuteAsModalOptions } from 'photoshop';
 import Spectrum from 'react-uxp-spectrum';
-
+import { InitLayers } from '../typescript/Metadata';
 
 export type InitPanelProps = { onFinished: () => void };
 
 export default function InitPanel({ onFinished }: InitPanelProps) {
-  const [state, setState] = React.useState({
-    CurrentValue: 0,
-  });
-
-  const maxProgressValue: number = app.activeDocument.layers.length;
+  const events: string[] = ['open', 'close'];
+  const [isInDocument, updateDocumentInUse] = useState(false);
 
   const Init = async () => {
-    console.log('init');
-    const options: ExecuteAsModalOptions = {commandName: "Writing metadata to all layers"};
+    const options: ExecuteAsModalOptions = { commandName: 'Writing metadata to all layers' };
     await core.executeAsModal(InitLayers, options);
     onFinished();
   };
 
+  const listener = async () => {
+    const isInDocument = app.activeDocument !== null;
+    updateDocumentInUse(isInDocument);
+  };
+
+  useEffect(() => {
+    action.addNotificationListener(events, listener);
+    updateDocumentInUse(app.activeDocument !== null);
+    return () => {
+      action.removeNotificationListener(events, listener);
+    };
+  });
+
+  if (isInDocument) {
+    return (
+      <div className="InitPanel">
+        {isInDocument && (
+          <Spectrum.Button variant="secondary" onClick={Init}>
+            {' '}
+            Start{' '}
+          </Spectrum.Button>
+        )}
+      </div>
+    );
+  }
+
   return (
     <div>
-      <sp-label>
-        Depending on how many layers are in the document. Initialize may take a few seconds and a progress popup may appear.
-      </sp-label>
-      <Spectrum.Button variant="secondary" onClick={Init}>Start</Spectrum.Button>
+      <Spectrum.Label> Plugin needs an open a document to run.</Spectrum.Label>
     </div>
   );
 }
