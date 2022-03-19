8f82b0183fd48155c7df18586affe36042c8cdcf
ScottWoodhams <scottwoodhams@hotmail.com>
2022-03-19 13:13:14 +0000

    consolidated checks into one object

--- src/Panels/ExportPanel.tsx
+++ src/Panels/ExportPanel.tsx
@@ -1,41 +1,22 @@
 import { app } from 'photoshop';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Spectrum from 'react-uxp-spectrum';
 import { ExportProcess } from '../typescript/Export';
-import { GetFonts, GetTextureCount } from '../typescript/Utilities';
-import {
-  ValidateNoEmptyLayers,
-  ValidateSliceValues,
-  ValidateTexts,
-  ValidateTexturesNames,
-} from '../typescript/Validation';
 
 export type ExportPanelProps = { onFinished: () => void };
 
 export function ExportPanel({ onFinished }: ExportPanelProps) {
-  const [hasDupeLayerNames, UpdateDupeLayerNames] = useState(false);
-  const [hasEmptyLayers, UpdateEmptyLayers] = useState(false);
-  const [hasEmptyTextLayers, UpdateEmptyTextLayers] = useState(false);
-  const [hasEmptySlices, UpdateEmptySlices] = useState(false);
-  const [textureCount, UpdateTextureCount] = useState(0);
-  const [fonts, UpdateFontCount] = useState([]);
+  // const emptyData: ValidationData = {
+  //   duplicateTextureNames: 0,
+  //   invalidTextLayers: 0,
+  //   invalidSlices: 0,
+  //   emptyLayers: 0,
+  // };
 
   async function Finish() {
     await ExportProcess();
     onFinished();
   }
-  useEffect(() => {
-    // todo add validation checks and is possible display a button to fix the issues
-    async function RunValidation() {
-      UpdateDupeLayerNames(await ValidateTexturesNames());
-      UpdateEmptyLayers(await ValidateNoEmptyLayers());
-      UpdateEmptyTextLayers(await ValidateTexts());
-      UpdateEmptySlices(await ValidateSliceValues());
-      UpdateTextureCount(await GetTextureCount());
-      UpdateFontCount(await GetFonts());
-    }
-    RunValidation();
-  });
 
   return (
     <div>
@@ -43,13 +24,6 @@ export function ExportPanel({ onFinished }: ExportPanelProps) {
       <Spectrum.Divider size="small" />
       <div className="validationBox">
         <sp-label>Total Layers: {app.activeDocument.layers.length}</sp-label>
-        <sp-label>Texture Count: {textureCount}</sp-label>
-        <sp-label> Has Dupes: {hasDupeLayerNames ? 'true' : 'false'} </sp-label>
-        <sp-label> Has Empty Layers: {hasEmptyLayers ? 'true' : 'false'} </sp-label>
-        <sp-label> Has Empty Text Layers: {hasEmptyTextLayers ? 'true' : 'false'} </sp-label>
-        <sp-label> Invalid Slices: {hasEmptySlices ? 'true' : 'false'} </sp-label>
-        <sp-label>Fonts: {fonts}</sp-label>
-        <sp-label>Has Dupes: {hasDupeLayerNames ? 'true' : 'false'}</sp-label>
       </div>
       <Spectrum.Divider size="small" />
       <Spectrum.ActionButton onClick={Finish}> Start Export</Spectrum.ActionButton>
--- src/typescript/Validation.ts
+++ src/typescript/Validation.ts
@@ -1,96 +1,69 @@
 import { action, app, Layer } from 'photoshop';
-import { IsTexture, IsTextureLayerObjKind } from './Utilities';
-import {GetMetaProperty, ReadFromMetaData} from './Metadata';
+import { IsTextureLayerObjKind } from './Utilities';
+import { GetMetaProperty, ReadFromMetaData } from './Metadata';
 import * as PSTypes from './PSTypes';
 
-export async function ValidateTexturesNames() {
-  let textureLayerNames = [];
+export interface ValidationData {
+  duplicateTextureNames: number;
+  emptyLayers: number;
+  invalidSlices: number;
+  invalidTextLayers: number;
+}
+
+export async function ValidationChecks(): Promise<ValidationData> {
+  let data: ValidationData;
+  const textureLayerNames = [];
   await Promise.all(
     app.activeDocument.layers.map(async (layer: Layer) => {
-      if (await IsTextureLayerObjKind(layer.kind)) {
-        textureLayerNames.push(layer.name);
+      const metadata = ReadFromMetaData(layer.id);
+
+      if (metadata !== undefined) {
+        if (await IsTextureLayerObjKind(layer.kind)) {
+          textureLayerNames.push(layer.name);
+
+          const slices = await GetMetaProperty(layer.id, 'Slices');
+          if (typeof slices === 'string') {
+            const slicesObj: PSTypes.Slices = JSON.parse(slices);
+
+            if (slicesObj.left === 0 && slicesObj.right === 0 && slicesObj.top === 0 && slicesObj.bottom === 0) {
+              data.invalidSlices++;
+            }
+          }
+        }
+        if (layer.bounds.width === 0 && layer.bounds.width === 0) {
+          data.emptyLayers++;
+        }
+
+        if (layer.kind === 'text') {
+          const result = await action.batchPlay(
+            [
+              {
+                _obj: 'get',
+                _target: [
+                  { _ref: 'layer', _id: layer.id },
+                  { _ref: 'document', _enum: 'ordinal' },
+                ],
+                _options: { dialogOptions: 'dontDisplay' },
+              },
+            ],
+            {
+              synchronousExecution: true,
+            }
+          )[0].textKey.textKey;
+
+          if (result === '' || result === undefined) {
+            data.invalidTextLayers++;
+          }
+        }
       }
     })
   );
 
   function findDuplicates(arr): Array<string> {
-    return arr.filter((item, index) => arr.indexOf(item) != index);
+    return arr.filter((item, index) => arr.indexOf(item) !== index);
   }
 
-  return findDuplicates(textureLayerNames).length > 0;
-}
+  data.duplicateTextureNames = findDuplicates(textureLayerNames).length;
 
-export async function ValidateNoEmptyLayers() {
-  let emptyLayers = [];
-  await Promise.all(
-    app.activeDocument.layers.map(async (layer: Layer) => {
-      if (layer.bounds.width === 0 && layer.bounds.width === 0) {
-        emptyLayers.push(layer);
-      }
-    })
-  );
-  return emptyLayers.length > 0;
-}
-
-export async function ValidateSliceValues() {
-  let invalidSlices = [];
-  await Promise.all(
-    app.activeDocument.layers.map(async (layer: Layer) => {
-      let slices = await GetMetaProperty(layer.id, 'Slices');
-      if (typeof slices === 'string') {
-        let slicesObj: PSTypes.Slices = JSON.parse(slices);
-
-        if (slicesObj.left === 0 && slicesObj.right === 0 && slicesObj.top === 0 && slicesObj.bottom === 0) {
-          invalidSlices.push(layer);
-        }
-      }
-    })
-  );
-
-  return invalidSlices.length > 0;
-}
-
-export async function ValidateTexts() {
-  let emptyTextLayers = [];
-  await Promise.all(
-    app.activeDocument.layers.map(async (layer: Layer) => {
-      if (layer.kind === 'text') {
-        const result = await action.batchPlay(
-          [
-            {
-              _obj: 'get',
-              _target: [
-                { _ref: 'layer', _id: layer.id },
-                { _ref: 'document', _enum: 'ordinal' },
-              ],
-              _options: { dialogOptions: 'dontDisplay' },
-            },
-          ],
-          {
-            synchronousExecution: true,
-          }
-        )[0].textKey.textKey;
-
-        if (result === '' || result === undefined) {
-          emptyTextLayers.push(layer);
-        }
-      }
-    })
-  );
-
-  return emptyTextLayers.length > 0;
-}
-
-export async function ValidateMeta() {
-  let metaLayers = [];
-  // todo check if there are 1 or more metadata layers available for export
-  await Promise.all(
-      app.activeDocument.layers.map(async (layer: Layer) => {
-        let meta = await ReadFromMetaData(layer.id);
-        if(meta !== undefined){
-          metaLayers.push(meta);
-        }
-      }));
-
-  return metaLayers.length;
+  return data;
 }
